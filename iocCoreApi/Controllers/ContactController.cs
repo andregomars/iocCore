2654f2f9f86a8ec55912bd9a988db8fb19e10ddd
Andre.W.Shen (tech.usca11.Newegg) 22513 <as13@buyabs.corp>
2016-06-28 15:10:15 -0700

    add post

--- iocCoreApi/Controllers/ContactController.cs
+++ iocCoreApi/Controllers/ContactController.cs
@@ -18,9 +18,18 @@ namespace iocCoreApi.Controllers
             this.contactRepository = new ContactRepository();
         }
 
+        //[Route("api/v1/contacts")]
         public Contact[] Get()
         {
             return contactRepository.GetAllContacts();
+        }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
         }
     }
 }
