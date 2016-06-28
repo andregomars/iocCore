d03fee322771ff949597ee8beabe4aecd67f14dc
andre <andregomars@gmail.com>
2016-06-28 01:02:47 -0700

    add test model and services

---
+++ iocCoreApi/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using iocCoreApi.Models;
+using iocCoreApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace iocCoreApi.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+    }
+}
