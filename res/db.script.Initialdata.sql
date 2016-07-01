7899e52cee71bdc6bae1ac7679b6fdccb349eb8a
andre <andregomars@gmail.com>
2016-06-30 23:49:48 -0700

    update sql

--- res/db.script.Initialdata.sql
+++ res/db.script.Initialdata.sql
@@ -21,4 +21,6 @@ Name
 select 'andre shen','andre','internal','0',1,'1234','626-111-1111',null,'andregomars@gmail.com',null,getdate(),'andre','Active'
 UNION ALL
 select 'carol liu','carol','byd','29051',1,'1234','909-111-1111',null,'carol@gmail.com',null,getdate(),'andre','Active'
+UNION ALL
+select 'iocdbo','ioc dbo','internal','0',1,'1234','626-222-2222',null,'iocdbo@gmail.com',null,getdate(),'andre','Active'
 
