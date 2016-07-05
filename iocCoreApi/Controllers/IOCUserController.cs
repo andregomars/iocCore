4208882a52c59f3363dba80c62b94827e49f4a53
andre <andregomars@gmail.com>
2016-07-04 23:42:33 -0700

    add ioc_user class

---
+++ iocCoreApi/Controllers/IOCUserController.cs
@@ -0,0 +1,53 @@
+﻿using iocCoreApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace iocCoreApi.Controllers
+{
+    public class IOCUserController : ApiController
+    {
+        //[Route("api/AuthenticateUser")]
+        //[ResponseType(typeof(Boolean))]
+        //public bool Get(string loginName, string password)
+
+        [ResponseType(typeof(IOC_User))]
+        public IHttpActionResult GetIOC_User(string loginName, string password)
+        {
+            IOC_User user = new IOC_User();
+            user.ID = 5;
+            user.allcaps = new Allcaps() { administrator = true };
+            user.cap_key = "ioc_capabilities";
+            user.caps = new Caps() { administrator = true };
+            user.data = new Data
+            {
+                ID = 5,
+                deleted = 0,
+                display_name = "iocdbo",
+                spam = 0,
+                user_activation_key = "",
+                user_email = "iocdbo@gmail.com",
+                user_login = "iocdbo",
+                user_nicename = "iocdbo",
+                //user_pass = "$P$BBh7gQRP.U9J3HOc03.hd4K0KFuShq0",
+                user_pass = "1234",
+                user_registered = "2016-06-05 00:16:33",
+                user_status = 0,
+                user_url = ""
+            };
+            user.filter = null;
+            user.roles = new List<string>() { "administrator" };
+
+            if (user.data.user_login == loginName && user.data.user_pass == password)
+            {
+                return Ok(user);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ iocCoreApi/Models/IOC_User.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iocCoreApi.Models
+{
+    public class IOC_User
+    {
+        public int ID { get; set; }
+        public Allcaps allcaps { get; set; }
+        public string cap_key { get; set; }
+        public Caps caps { get; set; }
+        public Data data { get; set; }
+        public object filter { get; set; }
+        public List<string> roles { get; set; }
+    }
+
+    public class Allcaps
+    {
+        public bool activate_plugins { get; set; }
+        public bool add_users { get; set; }
+        public bool administrator { get; set; }
+        public bool create_users { get; set; }
+        public bool delete_others_pages { get; set; }
+        public bool delete_others_posts { get; set; }
+        public bool delete_pages { get; set; }
+        public bool delete_plugins { get; set; }
+        public bool delete_posts { get; set; }
+        public bool delete_private_pages { get; set; }
+        public bool delete_private_posts { get; set; }
+        public bool delete_published_pages { get; set; }
+        public bool delete_published_posts { get; set; }
+        public bool delete_themes { get; set; }
+        public bool delete_users { get; set; }
+        public bool edit_dashboard { get; set; }
+        public bool edit_files { get; set; }
+        public bool edit_others_pages { get; set; }
+        public bool edit_others_posts { get; set; }
+        public bool edit_pages { get; set; }
+        public bool edit_plugins { get; set; }
+        public bool edit_posts { get; set; }
+        public bool edit_private_pages { get; set; }
+        public bool edit_private_posts { get; set; }
+        public bool edit_published_pages { get; set; }
+        public bool edit_published_posts { get; set; }
+        public bool edit_theme_options { get; set; }
+        public bool edit_themes { get; set; }
+        public bool edit_users { get; set; }
+        public bool export { get; set; }
+        public bool import { get; set; }
+        public bool install_plugins { get; set; }
+        public bool install_themes { get; set; }
+        public bool level_0 { get; set; }
+        public bool level_1 { get; set; }
+        public bool level_10 { get; set; }
+        public bool level_2 { get; set; }
+        public bool level_3 { get; set; }
+        public bool level_4 { get; set; }
+        public bool level_5 { get; set; }
+        public bool level_6 { get; set; }
+        public bool level_7 { get; set; }
+        public bool level_8 { get; set; }
+        public bool level_9 { get; set; }
+        public bool list_users { get; set; }
+        public bool manage_categories { get; set; }
+        public bool manage_links { get; set; }
+        public bool manage_options { get; set; }
+        public bool moderate_comments { get; set; }
+        public bool promote_users { get; set; }
+        public bool publish_pages { get; set; }
+        public bool publish_posts { get; set; }
+        public bool read { get; set; }
+        public bool read_private_pages { get; set; }
+        public bool read_private_posts { get; set; }
+        public bool remove_users { get; set; }
+        public bool switch_themes { get; set; }
+        public bool unfiltered_html { get; set; }
+        public bool unfiltered_upload { get; set; }
+        public bool update_core { get; set; }
+        public bool update_plugins { get; set; }
+        public bool update_themes { get; set; }
+        public bool upload_files { get; set; }
+    }
+
+    public class Caps
+    {
+        public bool administrator { get; set; }
+
+        public static implicit operator Caps(Allcaps v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Data
+    {
+        public int ID { get; set; }
+        public int deleted { get; set; }
+        public string display_name { get; set; }
+        public int spam { get; set; }
+        public string user_activation_key { get; set; }
+        public string user_email { get; set; }
+        public string user_login { get; set; }
+        public string user_nicename { get; set; }
+        public string user_pass { get; set; }
+        public string user_registered { get; set; }
+        public int user_status { get; set; }
+        public string user_url { get; set; }
+    }
+
+
+
+}
