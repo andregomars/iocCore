63733f11b27cd5050249ae05f2fe5e36bf77e263
andre <andregomars@gmail.com>
2016-07-07 00:36:36 -0700

    update login user

--- iocCoreApi/Controllers/IOCUserController.cs
+++ iocCoreApi/Controllers/IOCUserController.cs
@@ -19,20 +19,20 @@ namespace iocCoreApi.Controllers
         public IHttpActionResult GetIOC_User(string loginName, string password)
         {
             IOC_User user = new IOC_User();
-            user.ID = 5;
+            user.ID = 2;
             user.allcaps = new Allcaps() { administrator = true };
             user.cap_key = "ioc_capabilities";
             user.caps = new Caps() { administrator = true };
             user.data = new Data
             {
-                ID = 5,
+                ID = user.ID,
                 deleted = 0,
-                display_name = "iocdbo",
+                display_name = "carol liu",
                 spam = 0,
                 user_activation_key = "",
-                user_email = "iocdbo@gmail.com",
-                user_login = "iocdbo",
-                user_nicename = "iocdbo",
+                user_email = "carol@gmail.com",
+                user_login = "carol",
+                user_nicename = "carol liu",
                 //user_pass = "$P$BBh7gQRP.U9J3HOc03.hd4K0KFuShq0",
                 user_pass = "1234",
                 user_registered = "2016-06-05 00:16:33",
