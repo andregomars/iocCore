bdef28b7cf736dcce8b5384f52d896b08a695318
andre <andregomars@gmail.com>
2016-10-14 22:55:25 -0700

    update sms controller

---
+++ res/db.script.Initialdata.SMS.sql
@@ -0,0 +1,23 @@
+use IO_Online
+go
+
+insert into Core_SMS
+(MessageID
+,SubMessageID
+,SMSType
+,SenderCode
+,RecieverCode
+,Status
+,CreateTime
+,SendTime
+,Message)
+values 
+(null
+,null
+,1
+,'48507075'
+,'tel:+16262521073'
+,0
+,getdate()
+,null
+,'test go wild')
