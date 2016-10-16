97b2496fe2d5abf0a828b33177b3136966ea2065
andre <andregomars@gmail.com>
2016-10-16 11:26:51 -0700

    add dotnet core unit test

---
+++ iocCoreSMS/Models/OutboundSMSRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace iocCoreSMS.Models
+{
+    public class OutboundSMSRequest
+    {
+        public string address { get; set; }
+        public string message { get; set; }
+    }
+
+    public class OutboundSMSRequestWrapper
+    {
+        public OutboundSMSRequest outboundSMSRequest { get; set; }
+    }
+}
