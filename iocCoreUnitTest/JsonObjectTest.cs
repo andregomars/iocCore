49b441b4f83ea98a5fb80d3dcfd1d3f1f4754d81
andre <andregomars@gmail.com>
2016-10-16 21:49:00 -0700

    add test file

---
+++ iocCoreUnitTest/JsonObjectTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using iocCoreSMS.Models;
+
+namespace iocCoreUnitTest
+{
+    public class JsonObjectTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            var request = new OutboundSMSRequest {
+                address = "8002521111",
+                message = "sms message test"
+            }; 
+            var wrapper = new OutboundSMSRequestWrapper{
+                outboundSMSRequest = request
+            };
+            Assert.NotNull(wrapper);   
+        }
+    }
+}
