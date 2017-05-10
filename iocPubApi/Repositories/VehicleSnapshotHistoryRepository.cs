1123d0288d61137c712ba2b50154f86d75c69a3c
andregomars <andregomars@gmail.com>
2017-05-09 17:14:37 -0700

    fetch path from configuration

--- iocPubApi/Repositories/VehicleSnapshotHistoryRepository.cs
+++ iocPubApi/Repositories/VehicleSnapshotHistoryRepository.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Collections.Generic;
 using iocPubApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System.Linq;
 using Chilkat;
 
@@ -11,10 +12,16 @@ namespace iocPubApi.Repositories
     public class VehicleSnapshotHistoryRepository : IVehicleSnapshotHistoryRepository
     {
         private Csv csv;
-        private readonly string folder = "/Users/andre/projects/iocCore/res/pubapi/csv";
+        private string folder; //= "/Users/andre/projects/iocCore/res/pubapi/csv";
+        public IConfigurationRoot Configuration { get; }
 
         public VehicleSnapshotHistoryRepository()
         {
+            Configuration = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json").Build();
+
+            folder = Configuration["HAMS.Api:Directory.SMSData"];
         }
 
         IEnumerable<VehicleSnapshot> IVehicleSnapshotHistoryRepository.GetByVehicleName(string vname)
@@ -23,26 +30,13 @@ namespace iocPubApi.Repositories
             csv.HasColumnNames = true;
 
             bool success = csv.LoadFile(folder + "/snapshot.csv");
-            if(!success)
-            {
-                return null;
-            }
-
-            // VehicleSnapshot snapshot = new VehicleSnapshot {
-            //     code = "1E",
-            //     name = success.ToString(),
-            //     value = 80,
-            //     unit = "%",
-            //     time = new DateTime()
-            // };
-
-            var snapshot = new List<VehicleSnapshot>();
-
+            if(!success) return null;
+            var snapshots = new List<VehicleSnapshot>();
             int rowCount = csv.NumRows;
             int colCount = csv.NumColumns;
             for(int i = 0; i < rowCount; i++)
             {
-                snapshot.Add(new VehicleSnapshot{
+                snapshots.Add(new VehicleSnapshot{
                     code = csv.GetCellByName(i, "code"),
                     name = csv.GetCellByName(i, "name"),
                     value = Convert.ToDouble(csv.GetCellByName(i, "value")),
@@ -51,7 +45,7 @@ namespace iocPubApi.Repositories
                 });
             }
 
-            return snapshot;
+            return snapshots;
 
         }
 
