1bcefa4a851919aa62c85b1babff4dabca8c3b68
andregomars <andregomars@gmail.com>
2017-06-24 07:25:02 +0000

    add cache to snapshot controller

--- iocPubApi/Controllers/VehicleSnapshotController.cs
+++ iocPubApi/Controllers/VehicleSnapshotController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 using iocPubApi.Models;
 using iocPubApi.Repositories;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace iocPubApi.Controllers
 {
@@ -13,24 +14,50 @@ namespace iocPubApi.Controllers
     public class VehicleSnapshotController : Controller
     {
         private readonly IVehicleSnapshotRepository _repository;
+        private readonly IMemoryCache _cache;
+        private const string Key_VehicleSnapshot = "VehicleSnapshot_";
 
-        public VehicleSnapshotController(IVehicleSnapshotRepository repository)
+        public VehicleSnapshotController(IVehicleSnapshotRepository repository,
+            IMemoryCache memCache)
         {
             _repository = repository;
+            _cache = memCache;
         }
 
         // GET /api/VehicleSnapshot/GetByVehicleName/{vehicleName}
         [HttpGet("GetByVehicleName/{vehicleName}")]
         public IEnumerable<VehicleSnapshot> GetByVehicleName(string vehicleName)
         {
-            return _repository.GetByVehicleName(vehicleName);
+            IEnumerable<VehicleSnapshot> cacheEntry;
+            string cacheKey = Key_VehicleSnapshot + vehicleName;
+            if(!_cache.TryGetValue(cacheKey, out cacheEntry))
+            {
+                cacheEntry = _repository.GetByVehicleName(vehicleName);
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromMinutes(1));
+
+                _cache.Set(cacheKey, cacheEntry, cacheEntryOptions);
+            }
+
+            return cacheEntry;
         }
         
         // GET /api/VehicleSnapshot/GetWholeDayByVehicleName/{vehicleName}/{date}
         [HttpGet("GetWholeDayByVehicleName/{vehicleName}/{date}")]
         public IEnumerable<VehicleSnapshot> GetWholeDayByVehicleName(string vehicleName, DateTime date)
         {
-            return _repository.GetWholeDayByVehicleName(vehicleName, date);
+            IEnumerable<VehicleSnapshot> cacheEntry;
+            string cacheKey = Key_VehicleSnapshot + vehicleName + "_" + date.ToString("yyMMdd");
+            if(!_cache.TryGetValue(cacheKey, out cacheEntry))
+            {
+                cacheEntry = _repository.GetWholeDayByVehicleName(vehicleName, date);
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromMinutes(1));
+
+                _cache.Set(cacheKey, cacheEntry, cacheEntryOptions);
+            }
+
+            return cacheEntry;
         }
    }
 }
